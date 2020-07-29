3fe9c121b7fd734630872998a947958429c0ee11
letird <letidurigon@hotmail.com>
2020-07-28 23:32:13 -0300

    [ADD] #tutorial - Incluso campo para adicionar itens e ordenação para aparecer o último incluso como primeir

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -2,10 +2,30 @@ import { Template } from 'meteor/templating';
 
 import './body.html';
 
+import { Tasks } from '../api/tasks.js';
+
 Template.body.helpers({
-  tasks: [
-    {text: 'This is task 1'},
-    {text: 'This is task 2'},
-    {text: 'This is task 3'},
-  ],
+  tasks () {
+    return Tasks.find({}, {sort: {createdAt: -1}});
+  },
+});
+
+Template.body.events({
+  'submit .new-task' (event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
 });
