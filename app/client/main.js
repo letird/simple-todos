9c8496ea1b9b7046355a464e216dadd9cd73dc42
letird <letidurigon@hotmail.com>
2020-07-29 23:31:43 -0300

    [ADD] #tutorial - Finalizado o tutorial do meteor

--- app/client/main.js
+++ app/client/main.js
@@ -1,1 +1,2 @@
 import '../imports/ui/body.js';
+import '../imports/startup/accounts-config.js';
--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,12 +1,31 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './body.html';
+import './task.js';
 
 import { Tasks } from '../api/tasks.js';
 
+Template.body.onCreated(function bodyOnCreated () {
+  const self = this;
+  this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
+});
+
 Template.body.helpers({
   tasks () {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, {sort: {createdAt: -1}});
+  },
+
+  incompleteCount () {
+    return Tasks.find({checked: {$ne: true}}).count();
   },
 });
 
@@ -19,13 +38,22 @@ Template.body.events({
     const target = event.target;
     const text = target.text.value;
 
+    Meteor.call('tasks.insert', text);
+
     // Insert a task into the collection
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
     // Clear form
     target.text.value = '';
   },
+
+  'change .hide-completed input' (event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+
 });
